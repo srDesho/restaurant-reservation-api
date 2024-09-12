01d34aa9a61d4660cc2c16a76ea4ddbcddc6a826
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-09-12 01:48:20 -0400

    Initial commit

---
+++ restaurant-reservation-api/src/test/java/com/cristianml/reservation/RestaurantReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristianml.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReservationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
