ca57e2193246f5fb4762d8473edf9cd48b6ec979
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-09-25 23:15:57 -0400

    User entity created

---
+++ restaurant-reservation-api/src/main/java/com/cristianml/reservation/domain/entity/User.java
@@ -0,0 +1,31 @@
+package com.cristianml.reservation.domain.entity;
+
+import com.cristianml.reservation.domain.enums.Role;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
